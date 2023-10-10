4f6657a7864455e50e21591776a4327baa9d5c0c
Anuj7379 <124890802+Anuj7379@users.noreply.github.com>
2023-10-11 00:48:46 +0530

    Add files via upload

---
+++ insertion-sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int insertion_sort(int arr[], int size){
+    for (int i = i; i < size; i++)
+    {
+        int j = i-1;
+        int temp =arr[i];
+        for ( ;j >=0; j--)
+        {
+           
+            if (arr[j]>temp)
+            {
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+            
+        }
+        arr[j+1]=temp;
+        
+    }
+
+    for (int i = 0; i <size; i++)
+    {
+        cout<<arr[i];
+    }
+    
+    
+}
+int main(){
+    int arr[6] ={1,3,2,6,4,5};
+    insertion_sort(arr,6);
+    return 0;
+
+}
+/*
+
+time complexcity -- O(n^2)
+space compexcity --- O(1)
+
+*/
